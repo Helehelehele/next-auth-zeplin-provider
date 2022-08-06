e225cc0cc2f8176bbe21242e72410fa6aecc7686
Helehelehele <Helehelehele@users.noreply.github.com>
2022-08-06 17:03:10 +0300

    Upgrade to NextAuth v4

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
-interface ProviderZeplinOptions {
-    clientId: string;
-    clientSecret: string;
-}
+import type { OAuthConfig, OAuthUserConfig } from "next-auth/providers/oauth";
 
-export interface ZeplinProfile {
+export interface ZeplinProfile extends Record<string, unknown> {
     id: string;
     email: string;
     username: string;
@@ -11,20 +8,23 @@ export interface ZeplinProfile {
     avatar?: string;
 }
 
-export default (options: ProviderZeplinOptions): Record<string, unknown> => ({
-    id: "zeplin",
-    name: "Zeplin",
-    type: "oauth",
-    version: "2.0",
-    params: { grant_type: "authorization_code" },
-    accessTokenUrl: "https://api.zeplin.dev/v1/oauth/token",
-    authorizationUrl: "https://api.zeplin.dev/v1/oauth/authorize?response_type=code",
-    profileUrl: "https://api.zeplin.dev/v1/users/me",
-    profile: (profile: ZeplinProfile) => ({
-        id: profile.id,
-        name: profile.username,
-        email: profile.email,
-        image: profile.avatar || null
-    }),
-    ...options
-});
+export default function Zeplin<P extends ZeplinProfile>(
+    options: OAuthUserConfig<P>
+): OAuthConfig<P> {
+    return {
+        id: "zeplin",
+        name: "Zeplin",
+        type: "oauth",
+        version: "2.0",
+        accessTokenUrl: "https://api.zeplin.dev/v1/oauth/token",
+        authorization: { url: "https://api.zeplin.dev/v1/oauth/authorize?response_type=code" },
+        profileUrl: "https://api.zeplin.dev/v1/users/me",
+        profile: (profile: ZeplinProfile) => ({
+            id: profile.id,
+            name: profile.username,
+            email: profile.email,
+            image: profile.avatar || null
+        }),
+        ...options
+    };
+}
